2c612e07dcac09747a1e50e164582238cc50ffda
idupree <antispam@idupree.com>
2018-01-05 04:27:09 +0000

    implement serde for Vector

--- src/sequence/vector/mod.rs
+++ src/sequence/vector/mod.rs
@@ -13,6 +13,15 @@ use std::ops::Index;
 use std::iter::FromIterator;
 use std::mem::size_of;
 
+#[cfg(feature = "serde")]
+use serde::ser::{Serialize, Serializer, SerializeSeq};
+#[cfg(feature = "serde")]
+use serde::de::{Deserialize, Deserializer, SeqAccess, Visitor};
+#[cfg(feature = "serde")]
+use std::marker::PhantomData;
+#[cfg(feature = "serde")]
+use std::fmt;
+
 use utils::vec_utils::VecUtils;
 
 const DEFAULT_BITS: u8 = 5;
@@ -678,6 +687,54 @@ impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
 }
 
 impl<'a, T> ExactSizeIterator for Iter<'a, T> {}
+
+#[cfg(feature = "serde")]
+impl<A> Serialize for Vector<A> where A: Serialize {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut state = serializer.serialize_seq(Some(self.len()))?;
+        for item in self {
+            state.serialize_element(&item)?;
+        }
+        state.end()
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de, A> Deserialize<'de> for Vector<A> where A: Deserialize<'de> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_seq(VectorVisitor{phantom: PhantomData})
+    }
+}
+
+#[cfg(feature = "serde")]
+struct VectorVisitor<A> {
+    phantom: PhantomData<A>
+}
+
+#[cfg(feature = "serde")]
+impl<'de, A> Visitor<'de> for VectorVisitor<A>
+where A: Deserialize<'de>,
+{
+    type Value = Vector<A>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a sequence")
+    }
+
+    fn visit_seq<B>(self, mut seq: B) -> Result<Self::Value, B::Error>
+        where
+            B: SeqAccess<'de>,
+    {
+        let mut vector = Vector::new();
+
+        while let Some(value) = seq.next_element()? {
+            vector = vector.push_back(value);
+        }
+
+        Ok(vector)
+    }
+}
+
 
 #[cfg(test)]
 mod test;
